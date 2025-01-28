1c42821ef3ece00c3fb24910824b9d5ee77a9d6f
janithrijayodya <janithrijayodya@gmail.com>
2025-01-28 11:47:27 +0530

    completed logging and authontication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
@@ -7,9 +8,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('products/{product}/update', [ProductController::class, 'update'])->name('products.update');
-Route::delete('products/{product}/delete', [ProductController::class, 'delete'])->name('products.delete');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    
+    Route::get('/product', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('products/{product}/update', [ProductController::class, 'update'])->name('products.update');
+    Route::delete('products/{product}/delete', [ProductController::class, 'delete'])->name('products.delete');
+});
+
+require __DIR__.'/auth.php';
